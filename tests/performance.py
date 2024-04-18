7b2b3c737dc1eaa469d7cf6b8ea465bc12cee6c8
Simon Prato <simonprato11@gmail.com>
2024-04-18 21:13:28 +0100

    Import timeit to run it from root

---
+++ tests/performance.py
@@ -0,0 +1,44 @@
+"""Time it"""
+
+import timeit
+import sys
+
+from functions.recursive import floyd_warshall_recursive
+from functions.iterative import floydWarshall
+
+
+RUNS = 100
+
+
+def benchmark_floyd_warshall_recursive():
+    INF = sys.maxsize
+    graph = [
+        [0, 5, INF, 10],
+        [INF, 0, 3, INF],
+        [INF, INF, 0, 1],
+        [INF, INF, INF, 0],
+    ]
+    floyd_warshall_recursive(graph)
+
+
+def benchmark_floyd_warshall():
+    INF = sys.maxsize
+    graph = [
+        [0, 5, INF, 10],
+        [INF, 0, 3, INF],
+        [INF, INF, 0, 1],
+        [INF, INF, INF, 0],
+    ]
+    floydWarshall(graph)
+
+
+if __name__ == "__main__":
+    iterative_function_time = timeit.timeit(benchmark_floyd_warshall, number=RUNS)
+    recursive_function_time = timeit.timeit(
+        benchmark_floyd_warshall_recursive, number=RUNS
+    )
+    print(f"Time for recursive function: {recursive_function_time:.8f} seconds")
+    print(f"Time for iterative function : {iterative_function_time:.8f} seconds")
+
+
+# python -m tests.performance from root
